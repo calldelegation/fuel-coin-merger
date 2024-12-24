1033e079370e9d3d257a888ae2e157a7dfee8786
Daniel Bate <djbate23@gmail.com>
2024-12-24 01:22:42 +0000

    feat: mainnet support

--- src/lib.tsx
+++ src/lib.tsx
@@ -1,15 +1,14 @@
-export const environments = { LOCAL: "local", TESTNET: "testnet" };
+export const environments = { LOCAL: "local", TESTNET: "testnet", MAINNET: "mainnet" };
 export const environment =
   process.env.VITE_DAPP_ENVIRONMENT || environments.LOCAL;
 export const isLocal = environment === environments.LOCAL;
 export const isTestnet = environment === environments.TESTNET;
-
+export const isMainnet = environment === environments.MAINNET;
 export const localProviderUrl = `http://127.0.0.1:${process.env.VITE_FUEL_NODE_PORT || 4000}/v1/graphql`;
 export const testnetProviderUrl = "https://testnet.fuel.network/v1/graphql";
-export const providerUrl = isLocal ? localProviderUrl : testnetProviderUrl;
+export const mainnetProviderUrl = "https://mainnet.fuel.network/v1/graphql";
+export const providerUrl = isLocal ? localProviderUrl : isTestnet ? testnetProviderUrl : mainnetProviderUrl;
 export const playgroundUrl = providerUrl.replace("v1/graphql", "v1/playground");
-
-export const testnetFaucetUrl = "https://faucet-testnet.fuel.network/";
 
 export const renderTransactionId = (transactionId: string) => {
   if (isLocal) {
