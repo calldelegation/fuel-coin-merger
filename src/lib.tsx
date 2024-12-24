71ca3daecfae1ea3b8c1a638cd1a8632e2a02264
Daniel Bate <djbate23@gmail.com>
2024-12-24 11:24:41 +0000

    feat: mainnet

--- src/lib.tsx
+++ src/lib.tsx
@@ -7,7 +7,7 @@ export const isMainnet = environment === environments.MAINNET;
 export const localProviderUrl = `http://127.0.0.1:${process.env.VITE_FUEL_NODE_PORT || 4000}/v1/graphql`;
 export const testnetProviderUrl = "https://testnet.fuel.network/v1/graphql";
 export const mainnetProviderUrl = "https://mainnet.fuel.network/v1/graphql";
-export const providerUrl = isLocal ? localProviderUrl : isTestnet ? testnetProviderUrl : mainnetProviderUrl;
+export const providerUrl = isLocal ? localProviderUrl : mainnetProviderUrl;
 export const playgroundUrl = providerUrl.replace("v1/graphql", "v1/playground");
 
 export const renderTransactionId = (transactionId: string) => {
